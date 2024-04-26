8c1f2ab580ad90cbd801c76f78ca2859904dd9a6
Zaiik03 <openailillo2023@udec.cl>
2024-04-25 23:45:25 -0400

    Testeo funcionamiento y corrección de errores en Comprador

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Expendedor exp = new Expendedor(4);
-        Moneda m = new Moneda1000(); // hola
+        Expendedor exp = new Expendedor(10);
 
-        try{
-            Comprador c =  new Comprador(m, 7, exp);
-            System.out.println(c.queConsumiste()+", " + c.cuantoVuelto());
-        } catch (PagoIncorrectoException a){
-            System.out.println("Error de tipo pago: " + a.getMessage());
-        } catch (PagoInsuficienteException a){
-            System.out.println("Error de pago: " + a.getMessage() + ". Aqui tiene su dinero: " + m.getValor());
-        } catch (NoHayProductoException a){
-            System.out.println("Error de inventario: " + a.getMessage() + ". Aqui tiene su dinero: " + m.getValor());
+        for(int i = 0;i < 20;i++){
+            Moneda m = new Moneda1000();
+            try{
+                Comprador c =  new Comprador(m, Expendedor.COCA, exp);
+                System.out.println(c.queConsumiste()+", " + c.cuantoVuelto());
+            } catch (PagoIncorrectoException a){
+                System.out.println("Error de tipo pago: " + a.getMessage());
+            } catch (PagoInsuficienteException a){
+                System.out.println("Error de pago: " + a.getMessage() + ". Aqui tiene su dinero: " + m.getValor());
+            } catch (NoHayProductoException a){
+                System.out.println("Error de inventario: " + a.getMessage() + ". Aqui tiene su dinero: " + m.getValor());
+            }
         }
+
     }
 }
